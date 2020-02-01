890c7abc4171399a5da9d5f7401a36b33fb62756
Larry Chiem <clarry@pdx.edu>
2020-02-01 02:17:23 -0800

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './nike_PNG9.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="quote-container">
+          <p>Along with millions of athletes</p>
+          <p>and fans throughout the world,</p>
+          <p>we are devastated by</p>
+          <p>today's tragic news.</p>
+          <p>We extend our deepest</p>
+          <p>sympathies</p>
+          <p>to those closest to Kobe,</p>
+          <p>especially his family and friends.</p>
+          <p>He was one of the greatest</p>
+          <p>athletes of his generation and</p>
+          <p>has had an immeasurable impact</p>
+          <p>on the world of sport and the</p>
+          <p>community of basketball.</p>
+          <p>He was a beloved member</p>
+          <p>of the Nike family.</p>
+          <p>We will miss him greatly.</p>
+          <p>Mamba forever.</p>
+              <img src={logo} className={logo} alt={'kobe'}/>
+      </div>
   );
 }
 
