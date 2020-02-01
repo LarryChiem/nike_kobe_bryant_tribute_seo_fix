5b9bc8ddb452c6199da9abe11518bea329f6b189
Larry Chiem <clarry@pdx.edu>
2020-02-01 11:08:50 -0800

    Updates

---
+++ precache-manifest.4d6fd2e10fcd0b03e4f09947bc4b7fa5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92fdf1572e8af0cfa01cd824231a78e8",
+    "url": "/nike_kobe_bryant_tribute_seo_fix/index.html"
+  },
+  {
+    "revision": "f185661fff3cd0e1424a",
+    "url": "/nike_kobe_bryant_tribute_seo_fix/static/css/main.342b5544.chunk.css"
+  },
+  {
+    "revision": "98b6535831ac288de1e6",
+    "url": "/nike_kobe_bryant_tribute_seo_fix/static/js/2.30424f38.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nike_kobe_bryant_tribute_seo_fix/static/js/2.30424f38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f185661fff3cd0e1424a",
+    "url": "/nike_kobe_bryant_tribute_seo_fix/static/js/main.9dd7b13c.chunk.js"
+  },
+  {
+    "revision": "f48fdf79047703bc09eb",
+    "url": "/nike_kobe_bryant_tribute_seo_fix/static/js/runtime-main.5ad6a19c.js"
+  },
+  {
+    "revision": "46a3a239fd8759c5e0c76ac92b252ec7",
+    "url": "/nike_kobe_bryant_tribute_seo_fix/static/media/mamba-day-kobe-bryant-black-and-white-spotlight.46a3a239.jpg"
+  },
+  {
+    "revision": "b3434e3e703dc1cefa6052ace31234c1",
+    "url": "/nike_kobe_bryant_tribute_seo_fix/static/media/nike_PNG9.b3434e3e.png"
+  }
+]);
